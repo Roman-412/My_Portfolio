2d05dafb6797bdc3613e8da132bbc05a8131a65e
Roman-412 <romanranjee@gmail.com>
2026-01-04 15:29:34 +0530

    code

--- index.js
+++ index.js
@@ -6,23 +6,3 @@ var typed = new Typed(".text", {
     backDelay: 1000,
     loop: true
 });
-
-// Navbar Active Link on Scroll
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
-
-window.onscroll = () => {
-    sections.forEach(sec => {
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 150;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
-
-        if(top >= offset && top < offset + height) {
-            navLinks.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-            });
-        };
-    });
-};
