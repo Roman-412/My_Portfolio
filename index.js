a755f6cc1a8649d415f6605dfbefe308ae0d72b1
Roman-412 <romanranjee@gmail.com>
2026-03-31 15:54:15 +0530

    changes6

---
+++ index.js
@@ -0,0 +1,8 @@
+// Typing Animation
+var typed = new Typed(".text", {
+    strings: ["Frontend Designer", "Full Stack Developer", "UI/UX Specialist"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
